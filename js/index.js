4dd8de786f8bc0f5998636ffb799539edaa7b6c7
Christell-Mia <christell.romo@hotmail.com>
2022-11-19 09:46:04 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const $video = document.querySelector('#video')
+    const $play = document.querySelector('#play')
+    const $pause = document.querySelector('#pause')
+    const $backward = document.querySelector('#backward')
+    const $forward = document.querySelector('#forward')
+    $play.addEventListener('click', handlePlay)
+    $pause.addEventListener('click', handlePause)
+    function handlePlay(){
+        $video.play()
+        $play.hidden = true
+        $pause.hidden = false
+        console.log('le diste click al boton de play')
+    }
+    function handlePause(){
+        $video.pause()
+        $pause.hidden = true
+        $play.hidden = false
+        console.log('le diste pause')
+    }
+
+    $backward.addEventListener('click', handleBackward)
+    function handleBackward(){
+        $video.currentTime = $video.currentTime - 10
+        $video.play()
+        console.log("para atras 10 segundos", $video.currentTime)
+    }
+    $forward.addEventListener('click', handleForward)
+    function handleForward(){
+        $video.currentTime = $video.currentTime + 10
+        $video.play()
+        console.log("para adelante 10 segundos", $video.currentTime)
+    }
+
+    const $progress = document.querySelector('#progress')
+    $video.addEventListener('loadedmetadata', handleLoaded)
+    $video.addEventListener('timeupdate', handleTimeUpdate)
+    $progress.max = $video.duration
+    function handleLoaded(){
+        console.log('se ha cargado', $video.duration)
+    }
+    function handleTimeUpdate(){
+        $progress.value = $video.currentTime
+        // console.log("tiempo actal", $video.currentTime )
+    }
+
+    $progress.addEventListener('input', handleInput)
+    function handleInput(){
+        $video.currentTime = parseFloat($progress.value)
+
+        console.log($progress.value,)
+    }
